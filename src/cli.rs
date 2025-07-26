a9c2e33c2b6f7f6a2a1825b965467b11b9c367b9
Teppo Kavander <t.kavander@gmail.com>
2025-07-26 19:34:40 +0300

    chore: added auto coloring and examples

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,9 @@
-use clap::{Parser, Subcommand};
+use clap::{ColorChoice, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(name = "devbox", 
-about = "Developer's toolbox")]
+about = "Developer's toolbox",
+color = ColorChoice::Auto)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
@@ -11,12 +12,16 @@ pub struct Cli {
 #[derive(Subcommand)]
 pub enum Commands {
     /// Show public or local IP of this machine
+    #[command(
+    about = "Show public and/or local IP address.",
+    after_help = "Examples:\n  devbox ip \n  devbox ip --public \n  devbox ip -p"
+    )]
     Ip {
-        /// Show only public IP
+        /// Show only public IP address
         #[arg(short, long)]
         public: bool,
 
-        /// Show only local IP
+        /// Show only local IP address
         #[arg(short, long)]
         local: bool,
     },
