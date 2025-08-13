0b29081d9cda2bddedf96b3fda79178f9befdeae
Korppi <t.kavander@gmail.com>
2025-08-13 19:07:56 +0300

    feature: added pig art

--- src/commands/pigsay.rs
+++ src/commands/pigsay.rs
@@ -1,11 +1,19 @@
 use std::io::{Result, Write};
 
+const PIG: &str = r#"
+         _/|________
+        / o         \
+       E,            |S
+        \___________/
+         WW       WW
+"#;
+
 pub fn run(
     input: Option<String>,
     out: &mut dyn Write,
     err: &mut dyn Write,
 ) -> Result<()> {
-    
+    writeln!(out, "{PIG}")?;
         
     Ok(())
 }
