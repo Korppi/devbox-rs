a4144bf0c32bed8551eb6805a3174c5dcb10f02b
Korppi <t.kavander@gmail.com>
2025-08-17 00:19:11 +0300

    feature: speak bubble with hardcoded values added

--- src/commands/pigsay.rs
+++ src/commands/pigsay.rs
@@ -1,8 +1,10 @@
 use std::io::{Result, Write};
 
+use chrono::offset;
+
 use crate::utils::read_input;
 
-
+const bubble_offset: usize = 4;
 
 const PIG: &str = r#"
          _/|________
@@ -20,12 +22,23 @@ pub fn run(
     err: &mut dyn Write,
 ) -> Result<()>  {
 
+    let widht = 30; // TODO: as flag with default later
+
     let input_text = read_input(input)?;
-    let vec_text = textwrap::wrap(&input_text, 30);
+    let vec_text = textwrap::wrap(&input_text, widht);
     
+    let bubble_top = "_".repeat(widht + bubble_offset * 2);
+    writeln!(out, " {bubble_top}")?;
+    writeln!(out, "{}{}{}", "/", " ".repeat(widht + bubble_offset * 2), "\\")?;
+
     for text in vec_text{
-        writeln!(out, "{text}")?;
+        write!(out, "|{}", " ".repeat(bubble_offset))?;
+        write!(out, "{text}")?;
+        write!(out, "{}|", " ".repeat(bubble_offset  + widht-text.len()))?;
+        write!(out, "\n")?;
     }
+    writeln!(out, "{}{}  {}{}", "\\","_".repeat(3), "_".repeat(widht - 5 + bubble_offset * 2), "/")?;
+    write!(out, "    \\|")?;
 
     let pig = PIG;
     let pig = pig.replace("1", &eye.to_string());
